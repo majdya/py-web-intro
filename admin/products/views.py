9bd72d073b10e14cea157b44cc49ecf9937086e5
majdya <majd.ya.1995@gmail.com>
2025-01-28 01:11:00 +0200

    urls layout

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    def list(self, request): # GET/api/products/
+        print(request)
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request): # POST /api/products/
+            pass
+    
+    def retrieve(self, request, pk=None): # GET /api/products/<str:id>/
+         pass
+    
+    def update(self, request, pk=None): # GET /api/products/<str:id>/
+         pass
+    
+    def destroy(self, request, pk=None): # GET /api/products/<str:id>/
+         pass
