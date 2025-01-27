97dbc9671db133a479a9da6aba231c2b4b172b0d
majdya <majd.ya.1995@gmail.com>
2025-01-28 01:19:42 +0200

    get and post methods

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,status
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     def list(self, request): # GET/api/products/
-        print(request)
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
     def create(self, request): # POST /api/products/
-            pass
+            serializer = ProductSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     def retrieve(self, request, pk=None): # GET /api/products/<str:id>/
          pass
