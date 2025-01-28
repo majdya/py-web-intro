8abaded3f46b1f3ee60e8aca58af8f5e469dd9fb
majdya <majd.ya.1995@gmail.com>
2025-01-28 08:40:35 +0200

    django user and products app

--- admin/products/urls.py
+++ admin/products/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import ProductViewSet
+from .views import ProductViewSet,UserAPIView
 
 urlpatterns = [
     path('products', ProductViewSet.as_view({
         "get": 'list',
-        "post": 'create', })),
-    path('products/<str:pk>/', ProductViewSet.as_view({"get": 'retrive',"put": 'update', "delete": 'destroy', })),
+        "post": 'create'
+        })),
+    path('products/<str:pk>', ProductViewSet.as_view({
+        "get": 'retrieve',
+        "put": 'update',
+        "delete": 'destroy'
+        })),
+    path('user', UserAPIView.as_view())
 ]
